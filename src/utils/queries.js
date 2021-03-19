8c931e93519ba1c2d4f56ba2c1618490917b3ee8
Victor Teka <ambukateka@gmail.com>
2021-03-19 08:41:10 +0300

    Add graphql queries

---
+++ src/utils/queries.js
@@ -0,0 +1,39 @@
+import gql from 'graphql-tag'
+
+//Repos query
+export const SEARCH_FOR_REPOS = gql`
+    query($search_term: String!) {
+        search(query: $search_term, type: REPOSITORY, first: 20) {
+            repositoryCount,
+            edges {
+                node {
+                    ... on Repository {
+                        name,
+                        owner {
+                          login  
+                        },
+                        stargazers {
+                            totalCount
+                        },
+                        descriptionHTML,
+                    }
+                }
+            }
+        }
+    }
+`
+
+//Issues query
+export const GET_REPO_ISSUES = gql`
+    query($name: String!, $owner: String!) {
+        repository(name: $name, owner: $owner) {
+            issues(first: 20, states: [OPEN], orderBy: {field: CREATED_AT, direction: DESC}) {
+                nodes {
+                    title,
+                    bodyHTML,
+                    createdAt
+                }
+            }
+        }
+    }
+`
