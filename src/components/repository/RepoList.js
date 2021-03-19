d8bb2537f2d93eaea4836f0a28dbf8750a70aba9
Victor Teka <ambukateka@gmail.com>
2021-03-19 16:46:13 +0300

    fetch repositories from the server

--- src/components/repository/RepoList.js
+++ src/components/repository/RepoList.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import BounceLoader from 'react-spinners/BounceLoader'
 
-function RepoList() {
+import { searchRepo } from '../../actions'
+import { QUERY_REPOS } from '../../utils/queries'
+
+
+const RepoList = (props) => {
+
+    useEffect(()=>{
+        props.searchRepo(QUERY_REPOS('React'))
+    },[])
+
+    if(props.loading){
+        return(
+            <div>
+                <BounceLoader />
+            </div>
+        )
+    }
+
+    if(props.error){
+        return (
+            <div>
+                <h1>Error occured!</h1>
+            </div>
+        )
+    }
+    
     return (
         <div>
             <h1>Repo</h1>
@@ -8,4 +35,12 @@ function RepoList() {
     )
 }
 
-export default RepoList
+const mapStateToProps = (state)=>{
+    return {
+        loading: state.searchRes.loading,
+        error: state.searchRes.error,
+        repos: state.searchRes.repos
+    }
+}
+
+export default connect(mapStateToProps,{ searchRepo}) (RepoList)
