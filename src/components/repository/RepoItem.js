6662e6a93d46bcd2c82719dc13bbc28cb0f533ce
Victor Teka <ambukateka@gmail.com>
2021-03-21 07:10:06 +0300

    Show issue component when expandmore icon is clicked

--- src/components/repository/RepoItem.js
+++ src/components/repository/RepoItem.js
@@ -1,8 +1,11 @@
-import { Chip } from '@material-ui/core'
+import React from 'react'
+import { ExpansionPanelDetails } from '@material-ui/core'
+import { Chip, ExpansionPanel, ExpansionPanelSummary } from '@material-ui/core'
 import PeopleIcon from '@material-ui/icons/People'
 import StarIcon from '@material-ui/icons/Star'
-import React from 'react'
-import {Button,Image, Card } from 'semantic-ui-react'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import { Card } from 'semantic-ui-react'
 
 import IssueList from '../issues/IssueList'
 import './RepoItem.css'
@@ -11,19 +14,29 @@ function RepoItem({repo}) {
     const {node: {name, descriptionHTML, owner: {login}, stargazers:{totalCount: totalStarCount}}} = repo
     return (
         <div className='card__container'>
-        <Card className='card'>
-                <Card.Content>
+        <Card color='green' className='card'>
+            <ExpansionPanel>
+                <ExpansionPanelSummary>
+                <Card.Content  className='expand__panel' >
                     <div className='repo__header'>
-                    <Card.Header><h2>{name}</h2></Card.Header>
-                    <div>
-                        <Chip label={`By ${login}`} avatar={<PeopleIcon/>} className='chip' />
-                        <Chip label={totalStarCount} avatar={<StarIcon/>} className='chip' />         
-                    </div> 
+                        <Card.Header><h2>{name}</h2></Card.Header>
+                        <div>
+                            <Chip label={`By ${login}`} avatar={<PeopleIcon/>} className='chip' />
+                            <Chip label={totalStarCount} avatar={<StarIcon/>} className='chip' />         
+                        </div> 
                     </div>
-                    <Card.Description>
+                    <Card.Description className='repoDesc' >
                         <p className='repo__description' dangerouslySetInnerHTML={{__html:descriptionHTML}}></p>
                     </Card.Description>
+                <div className='expandIcon'>
+                    <ExpandMoreIcon />
+                </div>
                 </Card.Content>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <IssueList />
+                </ExpansionPanelDetails>
+                </ExpansionPanel>
             </Card>
         </div>
     )
