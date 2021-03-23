7aa4edf1f675fccaa41243efc4609b18791b34a9
Victor Teka <ambukateka@gmail.com>
2021-03-23 05:50:59 +0300

    chore: add some comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import searchRepoReducer from "./searchRepoReducer";
 
 const { combineReducers } = require("redux");
 
-
+//combine all the reducers together
 const rootReducer = combineReducers({
     searchRes: searchRepoReducer,
     issuesRes: issueReducer
